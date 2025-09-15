2dc6633ed7ceb9bfd8e2fef4ffc6e1246ac5b0aa
Nishant840 <nishant7896545@gmail.com>
2025-09-16 02:10:46 +0530

    development is completed

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pickle
+import numpy as np
 
 popular_df = pickle.load(open('popular.pkl','rb'))
+pivot_table = pickle.load(open('pivot_table.pkl','rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
+books = pickle.load(open('books.pkl','rb'))
 
 app = Flask(__name__)
 
@@ -14,6 +18,36 @@ def index():
                             votes = list(popular_df['num-ratings'].values),
                             rating = list(popular_df['avg-rating'].values)
                            )
+
+@app.route('/recommendation')
+def recommend_UI():
+    return render_template('recommend.html')
+
+@app.route('/recommend_books',methods=['POST'])
+def recommend():
+    user_input = request.form.get('user_input')
+
+    if user_input not in pivot_table.index:
+        return render_template(
+            'recommend.html',
+            data=[],
+            message=f"❌ Sorry, '{user_input}' is not in our dataset. Please try another book."
+        )
+
+    index = np.where(pivot_table.index == user_input)[0][0]
+    similar_items = sorted(list(enumerate(similarity_scores[index])),key = lambda x:x[1], reverse=True)[1:5]
+
+    data = []
+    for i in similar_items:
+        item = []
+        temp_df = books[books['Book-Title']==pivot_table.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+        
+        data.append(item)
+
+    return render_template('recommend.html',data=data, message=None)
 
 if __name__ == "__main__":
     app.run(debug=True)
